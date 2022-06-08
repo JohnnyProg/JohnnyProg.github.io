ed2be0e617acd572c00d3b2b4b995c9a48b3d9de
JohnnyProg <jangryta9@gmail.com>
2022-06-08 21:42:49 +0200

    fuck2

---
+++ js/test.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const arr = [document.getElementById("coffe"), document.getElementById("food"), document.getElementById("zestawy"), document.getElementById("form_zestaw"), document.getElementById("gallery")];
+    const arr2 = ["pages/kawy.html", "pages/jedzenie.html", "pages/zestaw_show.html", "pages/zestaw_forms.html", "pages/gallery.html"];
+    
+ for (let i = 0; i < arr.length; i++)
+ {
+     arr[i].addEventListener('click', function () {
+             fetch(arr2[i])
+                 .then(response => {
+                     return response.text();
+                 })
+                 .then(dane => {
+                     document.getElementById("mainPage").innerHTML = dane;
+                 })
+         },
+         false);
+ }
+})
