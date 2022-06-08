50c9b29b50567ef32198ba70ce6c72d388244035
JohnnyProg <jangryta9@gmail.com>
2022-06-08 21:34:37 +0200

    adding fetch

--- js/scripts.js
+++ js/scripts.js
@@ -25,5 +25,24 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const arr = [document.getElementById("coffe"), document.getElementById("food"), document.getElementById("zestawy"), document.getElementById("form_zestaw"), document.getElementById("gallery")];
+    const arr2 = ["pages/kawy.html", "pages/jedzenie.html", "pages/zestaw_show.html", "pages/zestaw_forms.html", "pages/gallery.html"];
+    
+ for (let i = 0; i < arr.length; i++)
+ {
+     arr[i].addEventListener('click', function () {
+             fetch(arr2[i])
+                 .then(response => {
+                     return response.text();
+                 })
+                 .then(dane => {
+                     document.getElementById("mainPage").innerHTML = dane;
+                 })
+         },
+         false);
+ }
+})
+
 
 
